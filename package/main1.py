0bd56bb118401bc6f6504933c7bcd65952b979b5
Nasnin c <nasninc4@gmail.com>
2025-11-06 10:59:07 +0530

    Add main1.py to demonstrate graphics functions

---
+++ package/main1.py
@@ -0,0 +1,9 @@
+import graphics.rectangle
+import graphics.circle
+import graphics.threeD_graphics.cuboid
+import graphics.threeD_graphics.sphere
+
+print("Rectangle area:",graphics.rectangle.area(10,5))
+print("Circle perimeter:",graphics.circle.perimeter(7))
+print("Cuboid surface area:",graphics.threeD_graphics.cuboid.surface_area(2,3,4))
+print("Sphere surface area:",graphics.threeD_graphics.sphere.surface_area(5))
